219a89b09b09776bed3ac88d18f41af55b44e401
Nada2111 <Nada151adel@gmail.com>
2023-06-05 17:17:38 -0700

    work please

--- 0x00-python-hello_world/list.h
+++ 0x00-python-hello_world/list.h
@@ -5,6 +5,7 @@
 
 /**
  * struct listint_s - singly linked list
+ * @n : integer
  * @next: points to the next node
  * Description: singly linked list node structure
  * for 0x00. Python - Hello, World
